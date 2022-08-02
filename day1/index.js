df2a73c763044c3736ee8e96d30c6b706032ef2e
Shubham Samantaray <samantaray.shubham423@gmail.com>
2022-08-02 20:47:23 +0530

    initial commit

---
+++ day1/index.js
@@ -0,0 +1,10 @@
+setTimeout(wakeUpUser, 5000);
+
+function wakeUpUser() {
+  alert("Why are you here, go back to study");
+}
+
+var circleRad = 20;
+var circleArea = Math.PI * (circleRad * circleRad);
+
+console.log(circleArea);
