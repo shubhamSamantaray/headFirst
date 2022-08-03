414470e9c2d92ba2e4755afd1933c132faa95ecc
Shubham Samantaray <samantaray.shubham423@gmail.com>
2022-08-03 20:29:26 +0530

    beer code

--- day1/index.js
+++ day1/index.js
@@ -1,10 +1,35 @@
-setTimeout(wakeUpUser, 5000);
+// setTimeout(wakeUpUser, 5000);
 
-function wakeUpUser() {
-  alert("Why are you here, go back to study");
+// function wakeUpUser() {
+//   // document.write("I was written first")
+// }
+
+// var circleRad = 20;
+// var circleArea = Math.PI * (circleRad * circleRad);
+
+// console.log(circleArea);
+// document.write("I was executed first")
+
+// var i = 0;
+// while (i < 2) {
+//   i = i + 1;
+//   document.write("Happy Birthday day to you.<br>");
+// }
+
+// var name = "joe";
+// document.write("Happy Birthday day dear " + name + ", <br>");
+// document.write("Happy Birthday day to you.<br>");
+var word = "bottles";
+var count = 99;
+
+while (count > 0) {
+  console.log(count + " " + word + " of beer on the wall");
+  console.log(count + " " + word + " of beer,");
+  console.log("Take one down, pass it around,");
+  count = count - 1;
+  if (count > 0) {
+    // console.log(count + " " + word + " of beer on the wall");
+  } else {
+    console.log("No more " + word + " of beer on the wall");
+  }
 }
-
-var circleRad = 20;
-var circleArea = Math.PI * (circleRad * circleRad);
-
-console.log(circleArea);
